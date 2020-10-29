86ba11bd76544baf2a1e82de514c660207ceee37
CherryWin456 <misuha96688@gmail,com>
2020-10-28 21:13:15 -0700

    first commit

---
+++ cha1.py
@@ -0,0 +1,28 @@
+name=input("Enter your name:")
+print("Hello "+ name)
+
+a=int(input("enter variable:"))
+b=int(input("enter another variable:"))
+print(a+b)
+
+C = int(input("enter Celsius temperature:"))
+F=9/5* C + 32
+print(F)
+
+second =int(input ("Enter second:"))
+Hours = second//3600
+print("Hours:" , Hours)
+Minutes=(second%3600)//60
+print("Minutes:",Minutes)
+Seconds=second%60
+print("Seconds:",Seconds)
+
+a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+a=a+b
+b=a-b
+a=a-b
+print("after swapping:")
+print("a=",a)
+print("b=",b)
+ 
